e2cc508703c257cb552e8df96261b8d8049f9e90
Evgeny Zemtsov <evan77@bk.ru>
2023-10-21 22:56:27 +0300

    Added fragment

---
+++ app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
@@ -0,0 +1,35 @@
+package ru.netology.nmedia.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import ru.netology.nmedia.R
+import ru.netology.nmedia.activity.NewPostFragment.Companion.textArg
+import ru.netology.nmedia.databinding.ActivityAppBinding
+
+class AppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        intent?.let {
+            if (it.action != Intent.ACTION_SEND) {
+                return@let
+            }
+
+            val text = it.getStringExtra(Intent.EXTRA_TEXT)
+            if (text.isNullOrBlank()) {
+                return@let
+            }
+            intent.removeExtra(Intent.EXTRA_TEXT)
+            findNavController(R.id.nav_host_fragment).navigate(
+                R.id.action_feedFragment_to_newPostFragment,
+                Bundle().apply {
+                    textArg = text
+                }
+            )
+        }
+    }
+}
--- app/src/main/java/ru/netology/nmedia/util/AndroidUtils.kt
+++ app/src/main/java/ru/netology/nmedia/util/AndroidUtils.kt
@@ -1,15 +1,18 @@
 package ru.netology.nmedia.util
 
 import android.content.Context
+import android.os.Bundle
 import android.view.View
 import android.view.ViewTreeObserver
 import android.view.inputmethod.InputMethodManager
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 object AndroidUtils {
-    fun hideKeyBoard(view: View) {
-        val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
-    }
+//    fun hideKeyBoard(view: View) {
+//        val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//        imm.hideSoftInputFromWindow(view.windowToken, 0)
+//    }
 
     fun View.focusAndShowKeyboard() {
         /**
@@ -46,4 +49,49 @@ object AndroidUtils {
                 })
         }
     }
+
+    fun getCountClick(cnt: Int): String {
+        val tmp: Int
+        val result: String = when {
+            cnt < 1000 -> {
+                "+$cnt"
+            }
+
+            cnt < 10_000 -> {
+                tmp = (cnt.toFloat() / 100.0f).toInt()
+                "+${String.format("%.1f", tmp.toFloat() / 10.0)}K"
+            }
+
+            cnt < 1_000_000 -> {
+                "+${(cnt.toFloat() / 1000.0f).toInt()}K"
+            }
+
+            else -> {
+                tmp = (cnt.toFloat() / 100_000.0f).toInt()
+                "+${String.format("%.1f", tmp.toFloat() / 10.0)}M"
+            }
+        }
+        return result
+    }
 }
+
+object StringArg : ReadWriteProperty<Bundle, String?> {
+
+    override fun setValue(thisRef: Bundle, property: KProperty<*>, value: String?) {
+        thisRef.putString(property.name, value)
+    }
+
+    override fun getValue(thisRef: Bundle, property: KProperty<*>): String? =
+        thisRef.getString(property.name)
+}
+
+object PostEditArg : ReadWriteProperty<Bundle, Int> {
+    override fun getValue(thisRef: Bundle, property: KProperty<*>): Int =
+        thisRef.getInt(property.name)
+
+    override fun setValue(thisRef: Bundle, property: KProperty<*>, value: Int) {
+        thisRef.putInt(property.name, value)
+    }
+
+}
+
