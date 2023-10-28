e02d6101992f1da77b8ec00f926a68ab206649c3
Evgeny Zemtsov <evan77@bk.ru>
2023-10-28 17:26:56 +0300

    added system time in field published

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -7,6 +7,8 @@ import ru.netology.nmedia.db.AppDb
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryImpl
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 private val empty = Post(
     id = 0,
@@ -34,13 +36,14 @@ class PostViewModel(application: Application) : AndroidViewModel(application) {
         edited.value?.let { post ->
             val text = content.trim()
             if (post.id != 0) {
-                if (text != post.content) repository.updateContent(post.id, text)
-            } else repository.save(post.copy(content = text))
+                if (text != post.content) repository.updateContent(post.id, text, getTime())
+            } else repository.save(post.copy(content = text, published = getTime()))
             edited.value = empty
         }
     }
 
     fun like(id: Int) = repository.like(id)
+
     fun remove(id: Int) = repository.removeById(id)
 
     fun share(id: Int) = repository.share(id)
@@ -48,4 +51,7 @@ class PostViewModel(application: Application) : AndroidViewModel(application) {
     fun cancelEdit() {
         edited.value = empty
     }
+
+    //@RequiresApi(Build.VERSION_CODES.O)
+    private fun getTime(): String = SimpleDateFormat("dd MMMM yyyy, HH:mm").format(Calendar.getInstance().time).toString()
 }
