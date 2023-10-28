9353a279a15dbf0442948440c75b7d1a73504486
Evgeny Zemtsov <evan77@bk.ru>
2023-10-28 10:54:14 +0300

    migrated on ROOM

--- app/src/main/java/ru/netology/nmedia/dao/PostDao.kt
+++ app/src/main/java/ru/netology/nmedia/dao/PostDao.kt
@@ -1,11 +1,44 @@
 package ru.netology.nmedia.dao
 
-import ru.netology.nmedia.dto.Post
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import ru.netology.nmedia.entity.PostEntity
 
+@Dao
 interface PostDao {
-    fun getAll(): List<Post>
-    fun save(post: Post): Post
+    @Query("SELECT * FROM PostEntity ORDER BY id DESC")
+    fun getAll(): LiveData<List<PostEntity>>
+
+    @Insert
+    fun insert(post: PostEntity)
+
+    @Query("UPDATE PostEntity SET content = :content WHERE id = :id")
+    fun updateContentById(id: Int, content: String)
+
+    fun save(post: PostEntity) =
+        if (post.id == 0) insert(post) else updateContentById(post.id, post.content)
+
+    @Query(
+        """
+        UPDATE PostEntity SET
+        countLike = countLike + CASE WHEN likeByMe THEN -1 ELSE 1 END,
+        likeByMe = CASE WHEN likeByMe THEN 0 ELSE 1 END
+        WHERE id = :id
+        """
+    )
     fun likeById(id: Int)
+
+    @Query("DELETE FROM PostEntity WHERE id = :id")
     fun removeById(id: Int)
+
+    @Query(
+        """
+        UPDATE PostEntity SET
+        countRepost = countRepost + 1
+        WHERE id = :id
+        """
+    )
     fun share(id: Int)
 }
--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,12 +1,9 @@
 package ru.netology.nmedia.dto
 
-import android.util.Log
-
 
 data class Post(
     val id: Int,
     val author: String,
-    //val authorAvatar: String = "",
     val published: String,
     val content: String,
     val likeByMe: Boolean,
